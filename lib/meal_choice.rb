ff2f05fc0061455c8de9950bd9797cf52317adf0
Hyojin Yoo <yoohyojin@gmail.com>
2020-04-22 21:03:42 +0000

    Done.

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,2 +1,13 @@
-# Your code here
-# For output purposes, use "puts" instead of "print" or "p"
+#Define a method named `meal_choice` that has **three** parameters:
+#- Two _required_ parameters for vegetables
+#- And an _optional_ parameter for `protein` with a default value of `tofu`
+#output a two messages and return one of the messages as a string
+#first message will always be the same: "What a nutritious meal!"
+#second message is structured to include all three parameters like so: "A plate of #{protein} with #{veg1} and #{veg2}."
+
+def meal_choice(veg1, veg2, protein = "tofu")
+  puts "What a nutritious meal!"
+  mssg = "A plate of #{protein} with #{veg1} and #{veg2}."
+  puts mssg
+  return mssg
+end
